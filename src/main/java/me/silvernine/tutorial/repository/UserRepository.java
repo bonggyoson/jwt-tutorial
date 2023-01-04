cdd479e2372f629c204341ac3968643892265538
bonggyoson <bonggyoson@gmail.com>
2023-01-04 21:34:55 +0900

    Initial commit

---
+++ src/main/java/me/silvernine/tutorial/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package me.silvernine.tutorial.repository;
+
+import me.silvernine.tutorial.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.persistence.Entity;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByUsername(String username);
+
+}
